477c4c6ddaff6570cd6d569a96b4233b471a47d8
WYT <tina.yiting.wei@gmail.com>
2021-04-20 17:08:03 +0800

    refactored attraction api, commented to explain the code

--- app.py
+++ app.py
@@ -45,82 +45,92 @@ def thankyou():
 ############# api #############
 ###############################
 
-##### get attractions with 2 query : page and keyword #####
+
+##### attraction ######
+
+# reusable function for create scene data from database search result
+def create_scene_data(result):
+    scene = {}
+    scene['id'] = result[0]
+    scene['name'] = result[1]
+    scene['category'] = result[2]
+    scene['description'] = result[3]
+    scene['address'] = result[4]
+    scene['transport'] = result[5]
+    scene['mrt'] = result[6]
+    scene['latitude'] = float(result[7])
+    scene['longitude'] = float(result[8])
+    scene['images'] = result[9].split(',')[:-1]
+    return scene
+
+# get attractions with 2 query : page and keyword 
 @app.route("/api/attractions")
 def get_attractions():
     try:
         page = int(request.args.get('page'))
         keyword = request.args.get('keyword')
         
+        # query without keyword
         if not keyword:
+            # database query 
             cursor.execute('SELECT * FROM attractions')
             results = cursor.fetchall()
             
+            # calculate total page and assign nextPage value
             page_count = cursor.rowcount // 12
             if page < page_count:
                 next_page = page + 1
             else:
                 next_page = None
             
+            # create the whole scenes list
             scene_list = []
-            
             for result in results:
-                scene = {}
-                scene['id'] = result[0]
-                scene['name'] = result[1]
-                scene['category'] = result[2]
-                scene['description'] = result[3]
-                scene['address'] = result[4]
-                scene['transport'] = result[5]
-                scene['mrt'] = result[6]
-                scene['latitude'] = float(result[7])
-                scene['longitude'] = float(result[8])
-                scene['images'] = result[9].split(',')[:-1]
-                
+                scene = create_scene_data(result)
                 scene_list.append(scene)
             
+            # parameters to specify start and end point for each data output page 
             start = page * 12
             end = (page + 1) * 12 
+            
+            # put processed data into response
             response = {
 				"nextPage": next_page,
 				"data": scene_list[start:end]
 			}
-            
+        
+        # query without keyword
         else:
+            # database query 
             keyword_query = f'SELECT * FROM attractions WHERE name LIKE "%{keyword}" OR category LIKE "%{keyword}%" OR description LIKE "%{keyword}%" OR address LIKE "%{keyword}%" OR mrt LIKE "%{keyword}%"'
             cursor.execute(keyword_query)
             results = cursor.fetchall()
             
+            # calculate total page and assign nextPage value
             page_count = cursor.rowcount // 12
             if page < page_count:
                 next_page = page + 1
             else:
                 next_page = None
             
+            # create the whole scenes list
             scene_list = []
             for result in results:
-                scene = {}
-                scene['id'] = result[0]
-                scene['name'] = result[1]
-                scene['category'] = result[2]
-                scene['description'] = result[3]
-                scene['address'] = result[4]
-                scene['transport'] = result[5]
-                scene['mrt'] = result[6]
-                scene['latitude'] = float(result[7])
-                scene['longitude'] = float(result[8])
-                scene['images'] = result[9].split(',')[:-1]
-                
+                scene = create_scene_data(result)
                 scene_list.append(scene)
 
-            
+            # parameters to specify start and end point for each data output page 
             start = page * 12
             end = (page + 1) * 12 
+            
+            # put processed data into response
             response = {
 				"nextPage": next_page,
 				"data": scene_list[start:end]
 			}
+            
         return make_response(jsonify(response),200)
+    
     except:
         return make_response(jsonify({
 			"error": True,
@@ -132,31 +142,25 @@ def get_attractions():
 @app.route('/api/attraction/<attractionId>')
 def get_attraction(attractionId):
     try:
+        # query without keyword
         id_query = f'SELECT * FROM attractions WHERE id={attractionId}'
         cursor.execute(id_query)
         result = cursor.fetchone()
+        
+        # incorrect attraction id
         if not result:
             return make_response(jsonify({"error":True,"message":"景點編號不正確"}),400)
         
-        scene = {}
-        scene['id'] = result[0]
-        scene['name'] = result[1]
-        scene['category'] = result[2]
-        scene['description'] = result[3]
-        scene['address'] = result[4]
-        scene['transport'] = result[5]
-        scene['mrt'] = result[6]
-        scene['latitude'] = float(result[7])
-        scene['longitude'] = float(result[8])
-        scene['images'] = result[9].split(',')[:-1]
-        
+        # get scene data and create response
+        scene = create_scene_data(result)
         response = {
             "data": scene
         }
+        
         return make_response(jsonify(response), 200)
     
     except:
         return make_response(jsonify({"error":True,"message": "伺服器內部出現錯誤"}),500)
-app.run(port=3000,debug=True)
 
-
+if __name__ == '__main__':
+    app.run(port=3000,debug=True)
