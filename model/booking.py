602af60e3f7b119239c96b5787b1fc7ce4242058
WYT <tina.yiting.wei@gmail.com>
2021-05-23 17:47:43 +0800

    remove redundant import

--- model/booking.py
+++ model/booking.py
@@ -1,20 +1,17 @@
 from model import db
 from model.models import Booking
-from model.models import Attraction
 
 def db_get_user_booking(user_id):
     query = f'''
     SELECT bookings.date, bookings.time, bookings.price, 
     attractions.id, attractions.name, attractions.address, attractions.images 
     FROM bookings
-    JOIN attractions on bookings.attraction_id = attractions.id 
+    JOIN attractions ON bookings.attraction_id = attractions.id 
     WHERE bookings.user_id = {user_id}
     '''
     results = db.engine.execute(query).fetchall()
     return results
     
-        
-
 
 def db_check_booking_exist(user_id,attr_id):
     all_user_bookings = Booking.query.filter_by(user_id=user_id).all()
